9685437441cb9b0bac5a2969e98c143c9911d859
IvanSD <ivansd122015@gmail.com>
2024-03-19 12:47:57 +0100

    DEterminar x canitdad de bolas

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.util.Random;
+import java.util.HashMap;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -23,7 +25,7 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numeroBalls)
     {
         int ground = 400;   // position of the ground line
 
@@ -33,20 +35,34 @@ public class BallDemo
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        int ballsCreadas = 0;
+        HashMap<Integer, BouncingBall> bolas = new HashMap();
+        Random random = new Random();
+        
+        while (ballsCreadas < numeroBalls){
+            int red = random.nextInt(256);
+            int green = random.nextInt(256);
+            int blue = random.nextInt(256);
+            Color color = new Color(red, green, blue);
+            BouncingBall ball = new BouncingBall(50, 50, random.nextInt(50-10)+10, color, ground, myCanvas);
+            ball.draw();
+            bolas.put(ballsCreadas, ball);
+            ballsCreadas ++;
+        }
+        
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            myCanvas.wait(50);// small delay
+            
+            for(int numeroBola : bolas.keySet()){
+                BouncingBall bola = bolas.get(numeroBola);
+                bola.move();
+                // stop once ball has travelled a certain distance on x axis
+                if(bola.getXPosition() >= 550){
+                    finished = true;
+                }
             }
         }
     }
